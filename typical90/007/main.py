d64c21bc71f8e9743fa5f98bb58fcb6ef463f61a
penicillin0 <ayahitonakamura@gmail.com>
2021-07-06 16:42:35 +0900

    007

--- typical90/007/main.py
+++ typical90/007/main.py
@@ -1,2 +1,17 @@
 import sys
+from bisect import bisect_left
 input = sys.stdin.readline
+
+n = int(input())
+A = list(map(int, input().split()))
+A.sort()
+Q = int(input())
+B = [int(input()) for _ in range(Q)]
+
+for b in B:
+    left = bisect_left(A, b) - 1
+    right = left + 1
+    if right > n - 1:
+        print(abs(b - A[left]))
+    else:
+        print(min(abs(b - A[left]), abs(b - A[right])))
