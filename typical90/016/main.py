da1bb4698d3b8ce51a00807a84066182d50f6eb9
penicillin0 <ayahitonakamura@gmail.com>
2021-08-23 20:20:20 +0900

    016

--- typical90/016/main.py
+++ typical90/016/main.py
@@ -1,2 +1,16 @@
 import sys
 input = sys.stdin.readline
+
+n = int(input())
+p, q, r = sorted(map(int, input().split()))
+
+
+m = 10 ** 4
+ans = float('inf')
+
+for a in range(m):
+    for b in range(m):
+        c = (n - a * p - b * q) // r
+        if n == a * p + b * q + c * r:
+            ans = min(ans, a + b + c)
+print(ans)
