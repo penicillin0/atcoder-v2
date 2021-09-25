4b349dfec920210047fb25096bb7e80d93e8595e
penicillin0 <ayahitonakamura@gmail.com>
2021-09-25 22:04:37 +0900

    076

--- typical90/076/main.py
+++ typical90/076/main.py
@@ -1,2 +1,27 @@
 import sys
 input = sys.stdin.readline
+
+N = int(input())
+A = list(map(int, input().split()))
+S = sum(A)
+
+double_A = A + A
+
+l, r = 0, 1
+cake_area = double_A[l]
+
+while r < 2 * N - 1:
+    # print("l, r, cake_area", l, r, cake_area)
+    if S == cake_area * 10:
+        print("Yes")
+        exit()
+
+    if cake_area * 10 > S:
+        cake_area -= double_A[l]
+        l += 1
+    else:
+        cake_area += double_A[r]
+        r += 1
+
+
+print("No")
